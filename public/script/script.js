76200e9acfe54b86817a1278275b193df2f92773
Joao Pedro <eng.joao.pedro@outlook.com>
2022-01-02 10:35:22 -0300

    adding element to cart

--- public/script/script.js
+++ public/script/script.js
@@ -1,5 +1,8 @@
 let carousel = document.querySelector('.carousel')
 
+//CRIAÇÃO DOS PRODUTOS AUTOMATICA
+updatePost()
+
 async function updatePost(){
         let resposta = await fetch('http://localhost:3000/api/all')
         let objetos = await resposta.json()
@@ -21,24 +24,95 @@ async function updatePost(){
         mural.innerHTML =  postElements
 }
 
-price()
+function idProduct(){
+    return Math.random().toString(36).substr(2, 9)
+} 
 
-async function price (){
+//CARRINHO DE COMPRAS
+telaCarrinho()
+removeDoCarrinho()
+finalizaCompra()
+
+async function telaCarrinho (){
     await updatePost()
 
     let prices = document.getElementsByClassName('price')
     for(let i = 0; i < prices.length; i++){
         let price = prices[i]
-        price.addEventListener('click', (event) => {
+            price.addEventListener('click', (event) => {
             let clicado = event.target
-            console.log(clicado)
+            let carrinho = document.querySelector('.carrinho')
+            let close = document.querySelector('.carrinho-icon')
+            if(clicado.className.toLowerCase() == 'price'){
+                carrinho.classList.add('mostra-carrinho')
+                adicionaCarrinho(clicado)
+            }else{
+                carrinho.classList.remove('mostra-carrinho')
+            }
+            close.addEventListener('click', () => {
+                carrinho.classList.remove('mostra-carrinho')
+            })
+            
         })
     }
 }
 
-function idProduct(){
-    return Math.random().toString(36).substr(2, 9)
-} 
+function adicionaCarrinho(clicado) {
+    let produto = clicado.parentElement
+    let img = produto.querySelector('.cap').src
+    let desc = produto.querySelector('.desc').innerText
+    let price = produto.querySelector('.price').innerText
+    let post = `<div class="carrinho-itens">
+                    <img src="${img}" class="item-imagem">
+                    <p class="item-nome">${desc}</p>
+                    <p class="item-valor">${price}</p>
+                    <input type="number" value="1" class="item-quantidade">
+                    <button class="item-remover">Remover</button>
+                </div>`
+    let carrinho = document.querySelector('.carrinho')
+    carrinho.innerHTML += post
+}
+
+async function removeDoCarrinho (){
+    await adicionaCarrinho()
+
+    let botaoRemove = document.getElementsByClassName('item-remover')
+    for(let i = 0; i < botaoRemove.length; i++){
+        let botao = botaoRemove[i]
+        botao.addEventListener('click', (event) => {
+            let clicado = event.target
+            clicado.parentElement.remove()
+            atualizaTotal()
+        })
+    }
+}
+
+function atualizaTotal () {
+    let carrinho = document.querySelector('.carrinho')
+    let compras = carrinho.getElementsByClassName('carrinho-itens')
+    let total = 0
+    for(let i = 0; i < compras.length; i++){
+        let carro = compras[i]
+        let priceElement = carro.querySelector('.item-valor')
+        let quantidadeElement = carro.querySelector('.item-quantidade')
+        let quantidade = quantidadeElement.value
+        let price = parseFloat(priceElement.innerText.replace('R$', ""))
+        total = total + (price * quantidade)
+    }
+    let geral = document.querySelector('.total')
+    geral.innerText = 'Total = ' + total
+}
+
+function finalizaCompra () {
+    let finaliza = document.querySelector('.finalizar')
+    finaliza.addEventListener('click', ()=> {
+        atualizaTotal()
+    })
+}
+
+//BOTOES CAROUSEL
+rightbtn()
+leftbtn()
 
 function rightbtn(){
     carousel.scrollLeft += carousel.offsetWidth
@@ -48,6 +122,7 @@ function leftbtn(){
     carousel.scrollLeft -= carousel.offsetWidth
 }
 
+//ANIMAÇÃO SKELETON
 skeleton()
 
 function skeleton(){
@@ -60,51 +135,4 @@ function skeleton(){
     }
 }
 
-function carrinhoCompras () {
-    carousel.addEventListener('click', (event) => {
-        let clicado = event.target
-        let carrinho = document.querySelector('.carrinho')
-        let close = document.querySelector('.carrinho-icon')
-        if(clicado.className.toLowerCase() == 'price'){
-            carrinho.classList.add('mostra-carrinho')
-            
-        }
-        close.addEventListener('click', () => {
-            carrinho.classList.remove('mostra-carrinho')
-        })
-    })
-}
 
-function removeCompra (){
-    let botaoRemove = document.getElementsByClassName('item-remover')
-    for(let i = 0; i < botaoRemove.length; i++){
-        let botao = botaoRemove[i]
-        botao.addEventListener('click', (event) => {
-            let clicado = event.target
-            clicado.parentElement.remove()
-        })
-    }
-}
-
-function atualizaTotal () {
-    let carrinho = document.querySelector('.carrinho')
-    let carros = carrinho.getElementsByClassName('carrinho-itens')
-    let total = 0
-    for(let i = 0; i < carros.length; i++){
-        let carro = carros[i]
-        let priceElement = carro.querySelector('.item-valor')
-        let quantidadeElement = carro.querySelector('.item-quantidade')
-        let quantidade = quantidadeElement.value
-        let price = parseFloat(priceElement.innerText.replace('R$', ""))
-        total = total + (price * quantidade)
-    }
-    let geral = document.querySelector('.total')
-    geral.innerText = 'Total = ' + total
-}
-
-function finaliza () {
-    let finaliza = document.querySelector('.finalizar')
-    finaliza.addEventListener('click', ()=> {
-        atualizaTotal()
-    })
-}
